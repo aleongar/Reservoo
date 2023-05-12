f4e7ff0759f7787a3d41ace6b4114f7b68425c3a
Álvaro León García <aleongarcia2002@gmail.com>
2023-05-12 13:48:04 +0200

    build hasta ahora

--- resources/views/angular.blade.php
+++ resources/views/angular.blade.php
@@ -1,13 +1,37 @@
-<!DOCTYPE html>
-<html lang="en"><head>
-    <meta charset="utf-8">
-    <title>Angular</title>
-    <base href="/">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" type="image/x-icon" href="favicon.ico">
-  <link rel="stylesheet" href="/assets/angular/styles.ef46db3751d8e999.css"></head>
-  <body>
-    <app-root></app-root>
-  <script src="/assets/angular/runtime.f3cc930729914d15.js" type="module"></script><script src="/assets/angular/polyfills.f91c42a6e04e503c.js" type="module"></script><script src="/assets/angular/main.21a082118c742c5a.js" type="module"></script>
-
-  </body></html>
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Angular</title>
+  <base href="/">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="favicon.ico">
+</head>
+<body>
+  <script>
+    function CSSDone(){
+            if($(window).width()>640){
+                $(".grid-item").css({"height":$(".grid-item").width()*0.2});
+                $(".grid_item_img").css({"height":$(".grid-item").width()*0.2});
+            console.log("a");
+            }else{
+                $(".grid-item").css({"height":$(".grid-item").width()*0.33});
+                $(".grid_item_img").css({"height":$(".grid-item").width()*0.33});
+            console.log("b");
+            }
+        }
+    document.addEventListener('readystatechange', function(e){
+      if(document.readyState == 'complete' && document.location.pathname.match(/\badmin\b/gm) != null){
+        console.log("cargando bootstrap");
+        var link = document.createElement('link');
+        link.setAttribute("rel", "stylesheet");
+        link.setAttribute("type", "text/css");
+        link.onload = CSSDone;
+        link.setAttribute("href", 'https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css');
+        document.getElementsByTagName("head")[0].appendChild(link);
+      }
+    })
+  </script>
+  <app-root></app-root>
+</body>
+</html>
